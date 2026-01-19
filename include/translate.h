f5ec15c1ebdbc224e4878574a45bcce036cc3e46
Patrick Fedick <patrick@pfp.de>
2026-01-19 13:03:32 +0100

    added translate and .clang-format

---
+++ include/translate.h
@@ -0,0 +1,41 @@
+#ifndef _TRANSLATE_H_
+#define _TRANSLATE_H_
+#include <ppl7.h>
+
+ppl7::String translate(const char *text);
+const ppl7::String &translate(const ppl7::String &text);
+const ppl7::String &translate(const ppl7::String &text, const ppl7::String &context);
+
+class Translator
+{
+  private:
+    ppl7::String lang;
+    ppl7::AssocArray data;
+
+  public:
+    class Speech
+    {
+      public:
+        ppl7::String audiofile;
+        ppl7::String text;
+        ppl7::String phonetics;
+        float timeout = 0.0f;
+        int id;
+        Speech(int id) : id(id) {};
+    };
+
+    std::map<ppl7::String, ppl7::String> languages;
+
+    Translator();
+    ~Translator();
+    void load();
+    void setLanguage(const ppl7::String &lang);
+    const ppl7::String &tr(const ppl7::String &text) const;
+    ppl7::String tr(const char *text) const;
+    Speech tr(int id) const;
+};
+
+Translator &GetTranslator();
+Translator::Speech translate(int id);
+
+#endif
---
+++ src/translate.cpp
@@ -0,0 +1,114 @@
+#include "translate.h"
+
+static Translator* translator=NULL;
+
+
+Translator& GetTranslator()
+{
+    return *translator;
+}
+
+ppl7::String translate(const char* text)
+{
+    if (translator) return translator->tr(text);
+    return ppl7::String(text);
+}
+
+const ppl7::String& translate(const ppl7::String& text)
+{
+    if (translator) return translator->tr(text);
+    return text;
+}
+
+Translator::Speech translate(int id)
+{
+    if (translator) return translator->tr(id);
+    return Translator::Speech(id);
+}
+
+
+Translator::Translator()
+{
+    translator=this;
+    lang="en";
+    languages.insert(std::pair<ppl7::String, ppl7::String>("en", "english"));
+    languages.insert(std::pair<ppl7::String, ppl7::String>("de", "german"));
+}
+
+Translator::~Translator()
+{
+    translator=NULL;
+}
+
+void Translator::load()
+{
+    data.clear();
+    ppl7::Dir dir;
+    if (!dir.tryOpen("lang")) {
+        return;
+    };
+    ppl7::Dir::Iterator it;
+    dir.reset(it);
+    ppl7::DirEntry e;
+    while (dir.getNextPattern(e, it, "*.json")) {
+        //ppl7::PrintDebug("loading language file: %s\n", (const char*)e.Filename);
+        ppl7::String filename=e.Path + "/" + e.Filename;
+        ppl7::AssocArray a;
+        try {
+            ppl7::File file(filename);
+            ppl7::Json::load(a, file);
+        } catch (const ppl7::Exception& exp) {
+            ppl7::PrintDebug("ERROR: could not load language file: %s\n", (const char*)e.Filename);
+            exp.print();
+        }
+        //a.list();
+        data+=a;
+    }
+    //data.list();
+
+}
+
+void Translator::setLanguage(const ppl7::String& lang)
+{
+    this->lang=lang;
+}
+
+const ppl7::String& Translator::tr(const ppl7::String& text) const
+{
+    if (lang.isEmpty()) return text;
+    ppl7::String path="ui/" + text;
+    //ppl7::PrintDebugTime("path=>>%s<<\n", (const char*)path);
+    if (data.exists(path)) {
+        const ppl7::AssocArray& node=data.getAssocArray(path);
+        if (node.exists(lang)) return node.getString(lang);
+        if (lang == "en") return text;
+    }
+    ppl7::PrintDebugTime("missing translation: ui => %s\n", (const char*)text);
+    return text;
+}
+
+ppl7::String Translator::tr(const char* text) const
+{
+    return tr(ppl7::String(text));
+}
+
+Translator::Speech Translator::tr(int id) const
+{
+    Translator::Speech speech(id);
+    ppl7::String path=ppl7::ToString("speech/%d", id);
+    if (data.exists(path)) {
+        const ppl7::AssocArray& node=data.getAssocArray(path);
+        if (node.exists("audiofile")) speech.audiofile=node.getString("audiofile");
+        if (node.exists("en/text")) speech.text=node.getString("en/text");
+        if (node.exists("en/phonetics")) speech.phonetics=node.getString("en/phonetics");
+        if (lang.notEmpty() && node.exists(lang) && lang != "en") {
+            const ppl7::AssocArray& lang_node=node.getAssocArray(lang);
+            if (lang_node.exists("text")) speech.text=lang_node.getString("text");
+            if (lang_node.exists("phonetics")) speech.phonetics=lang_node.getString("phonetics");
+        }
+    } else {
+        ppl7::PrintDebugTime("missing translation for id: %d\n", id);
+    }
+
+    return speech;
+}
