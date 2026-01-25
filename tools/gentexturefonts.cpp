7cd6c8dc6a0f8600615026b6dc43ac2953f59bde
Patrick Fedick <patrick@pfp.de>
2026-01-25 12:07:42 +0100

    generated texturefonts

---
+++ tools/gentexturefonts.cpp
@@ -0,0 +1,38 @@
+#include <ppl7.h>
+#include <ppl7-grafix.h>
+
+int main(int argc, char* argv[])
+{
+    ppl7::WideString letters = L"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ÄÖÜäöüß,.-;:_#'+*~^°!§$%&/()[]\\=?<>|@€";
+
+    ppl7::grafix::Grafix grafix;
+    ppl7::grafix::ImageFilter_PNG png;
+    grafix.loadFont("tmp/Source_Code_Pro/static/SourceCodePro-Regular.ttf", "SCP");
+
+    ppl7::grafix::Font font;
+    font.setName("SCP");
+    font.setOrientation(static_cast<ppl7::grafix::Font::Orientation>(static_cast<int>(ppl7::grafix::Font::Orientation::TOP) |
+                                                                     static_cast<int>(ppl7::grafix::Font::Orientation::LEFT)));
+    font.setSize(24);
+    font.setAntialias(true);
+    font.setColor(ppl7::grafix::Color(255, 255, 255, 255));
+
+    ppl7::grafix::Image img(64, 64, ppl7::grafix::RGBFormat::A8R8G8B8);
+
+    for (size_t i = 0; i < letters.length(); i++) {
+        img.cls(ppl7::grafix::Color(0, 0, 0, 0));
+        ppl7::WideString s = letters.mid(i, 1);
+        ppl7::PrintDebug("Rendering character: %lc\n", s[0]);
+        font.setColor(ppl7::grafix::Color(0, 0, 0, 255));
+        for (int y = 0; y <= 4; y++) {
+            for (int x = 0; x <= 4; x++) {
+                img.print(font, x, y + 24, s);
+            }
+        }
+        font.setColor(ppl7::grafix::Color(255, 255, 255, 255));
+        img.print(font, 2, 26, s);
+        png.saveFile(ppl7::ToString("res/fonts/scp/frame_%05u.png", s[0]), img);
+    }
+
+    return 0;
+}
