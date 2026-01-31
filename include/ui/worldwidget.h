ea9a7959a59404b2348568632afd2b7431e01dcc
Patrick Fedick <patrick@pfp.de>
2026-01-31 17:07:17 +0100

    Worldwidget abgespeckt

--- include/ui/worldwidget.h
+++ include/ui/worldwidget.h
@@ -5,58 +5,16 @@
 #include <ppl7-grafix.h>
 #include <ppltk.h>
 
-class Player;
-
-class StatsFrame : public ppltk::Widget
-{
-private:
-    ppl7::String labeltext;
-    ppl7::String value;
-    ppl7::grafix::Font font;
-
-public:
-    StatsFrame(int x, int y, int width, int height, const ppl7::String& label);
-    void setLabel(const ppl7::String& label);
-    void setValue(const ppl7::String& value);
-    ppl7::String widgetType() const override;
-    void paint(ppl7::grafix::Drawable& draw) override;
-    void setFontSize(int size);
-    const ppl7::String& label() const;
-};
-
-class OxygenFrame : public ppltk::Widget
-{
-    ppl7::String label;
-    float seconds_total, seconds_left;
-    ppl7::grafix::Font font;
-
-public:
-    OxygenFrame(int x, int y, int width, int height, const ppl7::String& label);
-    void setValue(float seconds_total, float seconds_left);
-    void setLabel(const ppl7::String& label);
-    ppl7::String widgetType() const override;
-    void paint(ppl7::grafix::Drawable& draw) override;
-    void setFontSize(int size);
-};
-
 class WorldWidget : public ppltk::Widget
 {
 private:
-    StatsFrame *stats_health, *stats_lifes, *stats_points;
-    OxygenFrame* stats_oxygen;
-    int value_health, value_lifes, value_points;
-    double oxygen_cooldown;
-    bool showui;
+    bool show_ui;
 
 public:
     WorldWidget();
-    void retranslateUi();
     void setViewport(const ppl7::grafix::Rect& viewport);
     ppl7::String widgetType() const override;
     void paint(ppl7::grafix::Drawable& draw) override;
-
-    void updatePlayerStats(const Player* player);
-    void resetPlayerStats(const Player* player);
     void setShowUi(bool enable);
 };
 
